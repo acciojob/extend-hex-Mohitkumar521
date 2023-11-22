12b43c3633452e08e4bff10155475eb37998b6ea
Mohit Kumar <109014039+Mohitkumar521@users.noreply.github.com>
2023-11-22 13:22:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 const extendHex = (shortHex) => {
-  // write your code here
+  // Remove the '#' if present
+  shortHex = shortHex.replace('#', '');
+
+  // Ensure the input is a valid short hex code
+  if (!/^[0-9a-fA-F]{3}$/.test(shortHex)) {
+    throw new Error('Invalid short hex code');
+  }
+
+  // Extend the short hex code to the full hex code
+  const fullHex = shortHex
+    .split('')
+    .map(char => char.repeat(2))
+    .join('');
+
+  // Add the '#' prefix and return the full hex code
+  return `#${fullHex.toUpperCase()}`;
 };
 
 // Do not change the code below.
 const shortHex = prompt("Enter Short Hex.");
 alert(extendHex(shortHex));
+
